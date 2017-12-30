1cf1399430a42e62ee6795e0b011419227179761
Reshabh Sharma <reshabhsh@gmail.com>
2017-12-30 13:42:19 +0530

    bug fix

--- source/crawler/gold.py
+++ source/crawler/gold.py
@@ -14,7 +14,7 @@ def gold():
     headers['Connection'] = 'keep-alive'
     headers['User-Agent'] = \
         'Mozilla/5.0 (X11; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0'
-    with urllib.request.urlopen("https://www.goldpriceindia.com") as conn:
+    with urllib.request.urlopen("tradesecret") as conn:
         rr=conn.read()
     a = BeautifulSoup(rr,'html.parser')
     b = a.select(".prc")
--- source/crawler/sensex.py
+++ source/crawler/sensex.py
@@ -14,7 +14,7 @@ def sensex():
     headers['Connection'] = 'keep-alive'
     headers['User-Agent'] = \
         'Mozilla/5.0 (X11; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0'
-    with urllib.request.urlopen("https://in.finance.yahoo.com/quote/%5EBSESN/history?p=%5EBSESN") as conn:
+    with urllib.request.urlopen("tradesecret") as conn:
         rr=conn.read()
     a = BeautifulSoup(rr,'html.parser')
     b = a.select(".BdT")
